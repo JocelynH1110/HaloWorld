6a10abb75f82be788f6d4fb409632d663afbfc60
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2023-09-08 00:38:23 +0800

    詢問使用者名字並回覆

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func greet(name string) {
+	if name == "" {
+		fmt.Println("HaloWorld")
+	} else {
+		fmt.Printf("Haloooo,%s\n", name)
+	}
+}
+
 func main() {
-	fmt.Println("HaaooloWorld")
+	var name string
+	fmt.Println("whats your name?")
+	fmt.Scanln(&name)
+	greet(name)
 }
