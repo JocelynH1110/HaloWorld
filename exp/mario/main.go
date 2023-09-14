0580a5238e2b8c96690b3cc5e023ca263df5446a
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2023-09-14 23:52:14 +0800

    refactor getting height

--- exp/mario/main.go
+++ exp/mario/main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+func getHeight() int {
+	var height int
+	fmt.Print("Height: ")
+	fmt.Scanf("%d", &height)
+	return height
+}
 func main() {
-	var height int = 3
+	var height int = getHeight()
+
 	for y := 1; y <= height; y++ {
 
 		for x := 1; x <= height-y; x++ {
