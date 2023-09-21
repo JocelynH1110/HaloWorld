2c26f5c0f354f89aa22fef8241e48774c395ea92
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2023-09-21 13:58:34 +0800

    求1-10質數

--- exp/prime_number/main.go
+++ exp/prime_number/main.go
@@ -2,48 +2,53 @@ package main
 
 import "fmt"
 
-func isPrime(num3 int) bool {
+func isPrime(num int) bool {
 
-	if num3 <= 1 {
+	if num <= 1 {
 		return false
 	}
-	for i := num; i < num2; i++ {
-		if num2%i == 0 {
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
 			return false
 		}
 	}
-
+	
 	return true
 }
 
-func getNumber(num, num2 int) []int {
-	primes := []int{}
-	for i := num; i <= num2; i++ {
-		if isPrime(i) {
-			primes = append(primes, i)
+// func getNumber(num, num2 int) []int {
+// 	primes := []int{}
+// 	for i := num; i <= num2; i++ {
+// 		if isPrime(i) {
+// 			primes = append(primes, i)
 
-		}
+// 		}
 
-	}
-	return primes
-}
+// 	}
+// 	return primes
+// }
+
 func main() {
-
-	var num, num2 int
-	for {
-		fmt.Print("Minimun: ")
-		fmt.Scanf("%d", &num)
-
-		fmt.Print("Maximun: ")
-		fmt.Scanf("%d", &num2)
-
-		if num > num2 {
-			return
-		} else if num < 1 {
-			return
+	for i:=1;i<=10;i++{
+		if isPrime(i){
+			fmt.Println(i)
 		}
-
 	}
-	fmt.Printf("%v", getNumber)
+	// for {
+	// 	fmt.Print("Minimun: ")
+	// 	fmt.Scanf("%d", &num)
+
+	// 	fmt.Print("Maximun: ")
+	// 	fmt.Scanf("%d", &num2)
+
+	// 	if num > num2 {
+	// 		return
+	// 	} else if num < 1 {
+	// 		return
+	// 	}
+
+	// }
+
+	//fmt.Printf()
 
 }
