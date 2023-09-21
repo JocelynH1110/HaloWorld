eafde2ca5bed2f4ee9175361ee11d94161ed6ee7
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2023-09-21 22:21:57 +0800

    抓取輸入值之間的質數

--- exp/prime_number/main.go
+++ exp/prime_number/main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func isPrime(num int) bool {
-
 	if num <= 1 {
 		return false
 	}
@@ -12,43 +11,20 @@ func isPrime(num int) bool {
 			return false
 		}
 	}
-	
 	return true
 }
 
-// func getNumber(num, num2 int) []int {
-// 	primes := []int{}
-// 	for i := num; i <= num2; i++ {
-// 		if isPrime(i) {
-// 			primes = append(primes, i)
-
-// 		}
-
-// 	}
-// 	return primes
-// }
-
 func main() {
-	for i:=1;i<=10;i++{
-		if isPrime(i){
+	var min, max int
+	fmt.Print("Minimum: ")
+	fmt.Scanf("%d", &min)
+
+	fmt.Print("Maximum: ")
+	fmt.Scanf("%d", &max)
+
+	for i := min; i <= max; i++ {
+		if isPrime(i) {
 			fmt.Println(i)
 		}
 	}
-	// for {
-	// 	fmt.Print("Minimun: ")
-	// 	fmt.Scanf("%d", &num)
-
-	// 	fmt.Print("Maximun: ")
-	// 	fmt.Scanf("%d", &num2)
-
-	// 	if num > num2 {
-	// 		return
-	// 	} else if num < 1 {
-	// 		return
-	// 	}
-
-	// }
-
-	//fmt.Printf()
-
 }
