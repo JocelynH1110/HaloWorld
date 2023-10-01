dc68336db7ea1926e5c6568ffab6fd05e0754d33
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2023-10-02 00:06:29 +0800

    credit 卡號產生

---
+++ exp/credit/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "log"
+import "errors"
+
+const CARD_NUMBER string = "4003600000000014"
+
+func stringToDigits(str string) ([]int, error) {
+
+	digits := make([]int, 0)
+
+	for _, chr := range str {
+		if chr < 48 || chr > 57 {
+			err := errors.New("不正確的數字")
+			return []int{}, err
+		}
+		digit := int(chr) - 48
+		digits = append(digits, digit)
+
+	}
+
+	return digits, nil
+}
+func main() {
+	digits, err := stringToDigits(CARD_NUMBER)
+
+	if err != nil {
+		log.Fatalln("輸入錯誤")
+	}
+
+	for _, digit := range digits {
+
+		fmt.Println(digit)
+	}
+}
